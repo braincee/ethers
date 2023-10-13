4a770a867f0bb6033d09922f58b6db73b3bf5a76
stphndev <133531810+stphndev@users.noreply.github.com>
2023-10-13 14:45:27 +0000

    Add codes

--- new.cjs
+++ new.cjs
@@ -11,7 +11,13 @@ const signer = new ethers.Wallet(privateKey, provider);
 const rawTransaction = process.env.SIGNED_CONTRACT;
 
 async function sendTransaction() {
-  const transaction = await signer.populateTransaction(rawTransaction);
+  const transaction = await ethers.utils.parseTransaction(rawTransaction);
+
+  console.log('To:', transaction.to);
+  console.log('From:', transaction.from);
+  console.log('Amount:', ethers.utils.formatUnits(transaction.value, 'ether'), 'ETH');
+  console.log('Gas Price:', ethers.utils.formatUnits(transaction.gasPrice, 'gwei'), 'gwei');
+  console.log('Gas Limit:', transaction.gasLimit.toString());
 
   signer.sendTransaction(transaction)
     .then((tx) => {
